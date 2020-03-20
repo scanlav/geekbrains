52e70c430507ba173c786d60ad298e143eb39f2f
scanlav <scanlave@gmail.com>
2020-03-20 16:19:36 +0800

    04_02

--- homework4_02.py
+++ homework4_02.py
@@ -4,5 +4,5 @@
 # генератор.
 
 my_list = [3, 20, 9, 5, 1, 8, 11, 6, 15]
-new_list= [i for i in my_list if i > my_list[my_list.index(i)-1]]
+new_list = [i for i in my_list if i > my_list[my_list.index(i)-1]]
 print(new_list)
